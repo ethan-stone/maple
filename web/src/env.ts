24c79284111950d4060181739d89f0f1a0e49e31
ethan-stone <ethan.stone9352@gmail.com>
2023-05-20 23:52:59 -0400

    installed packages and setup clerk provider and env

---
+++ web/src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    CLERK_SECRET_KEY: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
+});
