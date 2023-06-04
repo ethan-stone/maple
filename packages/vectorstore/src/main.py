e20e7401f00e311e8724dcb36ff1b286cb0e48a8
ethan-stone <ethan.stone9352@gmail.com>
2023-06-03 23:55:05 -0400

    learned discriminated unions in pydantic

--- packages/vectorstore/src/main.py
+++ packages/vectorstore/src/main.py
@@ -1,2 +1,28 @@
+from typing import Literal
+from pydantic import BaseModel, conlist, Field
+
+
+class Vector(BaseModel):
+    id: str
+    vector: conlist(float, min_items=1536, max_items=1536)
+    metadata: dict
+
+
+class Query(BaseModel):
+    command: Literal["query"]
+    vector: conlist(float, min_items=1536, max_items=1536)
+
+
+class Upsert(BaseModel):
+    command: Literal["upsert"]
+    vectors: list[Vector]
+
+
+class Event(BaseModel):
+    evt: Upsert | Query = Field(..., discriminator="command")
+
+
 def handler(event, context):
+    evt = Event(evt=event)
+
     print("Hello from vectorstore!")
