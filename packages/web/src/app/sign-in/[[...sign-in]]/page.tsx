151b4e6f9d88534b3fc9212d57f3d2ae855f4a8d
ethan-stone <ethan.stone9352@gmail.com>
2023-10-18 18:21:39 -0400

    remove web

--- packages/web/src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <SignIn redirectUrl="/notes" />
-    </main>
-  );
-}
--- packages/web/src/app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <SignUp redirectUrl="/notes" />
-    </main>
-  );
-}
